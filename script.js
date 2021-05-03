bafff3629a379e36b3ae3fe921405436da507d45
aj <404americajones@gmail.com>
2021-05-03 13:21:11 -0600

    styling and dimensions fix

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const boxes = document.querySelectorAll('.box');
+let i = 0;
+boxes.forEach(box => {
+    box.setAttribute('id', i);
+    i++;
+})
 
 function dragBox(box) {
-    var pos1=0, pos2=0, pos3=0, pos4=0;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     box.onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
-        e=e || window.event;
+        e = e || window.event;
         e.preventDefault();
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
     }
-    
+
     function elementDrag(e) {
-        e=e || window.event;
+        e = e || window.event;
         e.preventDefault();
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
@@ -22,6 +27,7 @@ function dragBox(box) {
         pos4 = e.clientY;
         box.style.top = (box.offsetTop - pos2) + "px";
         box.style.left = (box.offsetLeft - pos1) + "px";
+        console.log(pos3, pos4);
     }
 
     function closeDragElement() {
@@ -30,16 +36,44 @@ function dragBox(box) {
     }
 }
 
+
 function rotateBox() {
     if (this.classList.contains('rotated')) {
-        console.log('ALREADY ROTATED');
-        // this.style.transform = 'rotate(90deg)';
-        this.classList.remove('rotated')
+        this.classList.remove('rotated');
+        this.classList.add('rotatedAgain');
+    } else if (this.classList.contains('rotatedAgain')) {
+        this.classList.remove('rotatedAgain');
+        this.classList.add('rotatedAgainAgain');
+    } else if (this.classList.contains('rotatedAgainAgain')) {
+        this.classList.remove('rotatedAgainAgain');
     } else {
         // this.style.transform = 'rotate(90deg)';
         this.classList.add('rotated');
+    }
 }
-}
+
+// let save = document.querySelector('.save');
+// save.addEventListener('click', localSave);
+
+// function localSave() {
+
+//     boxes.forEach(box => {
+//         console.log(box.id);
+//         console.log(box.classlist);
+//         console.log('TOP NUM??: ', box.style.offsetTop)
+//         id= box.id;
+//         // localStorage.setItem(id, location)
+//     })
+// }
+// let load = document.querySelector('.load');
+// load.addEventListener('click', rearrange);
+
+//get classlist of each item so i can reassign classes on load
+//get style top and left px
+
+// function rearrange() {
+
+// }
 
 boxes.forEach(box => {
     dragBox(box);
